96041380d02409420ac568c08d952fb566abb617
payne <harrison.payne@gmail.com>
2024-10-12 00:20:41 -0600

    kosaraju fn docstring and dummy return

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     let gr: DiGraph<u32,bool> = Graph::<u32, bool, Directed>::with_capacity(11, 16);
 }
 
-fn kosaraju(gr: DiGraph<u32,bool>) {
-
+/// Returns sizes of top five strongly connected components.
+fn kosaraju(gr: DiGraph<u32,bool>) -> (u32, u32, u32, u32, u32) {
+    (0, 0, 0, 0, 0)
 }
