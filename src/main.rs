c240bcdfdf859d390d2f6eeb5b14489626439e89
payne <harrison.payne@gmail.com>
2024-10-15 16:50:17 -0600

    Implement topo_sort via Kahn's algo

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 use petgraph::graph::{DiGraph, Graph, NodeIndex};
 use petgraph::visit::EdgeRef;
 use petgraph::Directed;
+use petgraph::Incoming;
 
 fn main() {
     let mut gr: DiGraph<usize, bool> = Graph::<usize, bool, Directed>::with_capacity(11, 16);
@@ -65,11 +66,38 @@ fn graph_reverse(gr: &DiGraph<usize, bool>, nodes: &[Option<NodeIndex>]) -> DiGr
     rev_gr
 }
 
-/// Returns graph with node weights in a topological order
-fn topo_sort(gr: &DiGraph<usize, bool>, nodes: &[Option<NodeIndex>]) -> DiGraph<usize, bool> {
-    gr.clone()
+/// Returns graph with node weights in a topological order.
+/// Using Kahn's algorithm
+fn topo_sort(gr: &DiGraph<usize, bool>, nodes: &[Option<NodeIndex>]) -> Vec<NodeIndex> {
+    // stack for keeping track of nodes with no incoming edge
+    let mut stack: Vec<NodeIndex> = Vec::with_capacity(gr.node_count());
+    // list for keeping track of nodes in their final order
+    let mut order: Vec<NodeIndex> = Vec::with_capacity(gr.node_count());
+    // populate stack with existing no-incoming-edge nodes
+    for node in nodes {
+        if gr.edges_directed(node.unwrap(), Incoming).nth(0) == None {
+            stack.push(node.unwrap());
+        }
+    }
+    // make mutable copy of gr for removing edges from
+    let mut gr_cp = gr.clone();
+    // main body of algorithm
+    while !stack.is_empty() {
+        let n = stack.pop();
+        order.push(n.unwrap());
+        for e in gr.edges(n.unwrap()) {
+            let m = e.target();
+            gr_cp.remove_edge(e.id());
+            if gr.edges_directed(m, Incoming).nth(0) == None {
+                stack.push(m);
+            }
+        }
+    }
+    order
 }
 
+// I'm keeping this around to demonstrate I understand DFS, however,
+// I ended up using Khan's algorithm instead.
 /// Performs a depth first search on a graph. Does nothing.
 fn DFS_iter(gr: &DiGraph<usize, bool>, nodes: &[Option<NodeIndex>]) {
     // stack for keeping track of nodes to visit next
